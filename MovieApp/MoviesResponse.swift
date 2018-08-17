550dff908ef405f7c22158f8ae32ca59f2d8d2f2
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2018-08-17 01:01:51 -0400

    Update MoviesResponse with optional values

--- MovieApp/MoviesResponse.swift
+++ MovieApp/MoviesResponse.swift
@@ -4,16 +4,16 @@
 //
 
 struct MoviesResponse: Decodable {
-    let results: [Movie]
+    let results: [Movie]?
 }
 
 struct Movie: Decodable {
-    let adult: Bool
-    let backdropPath: String
-    let id: Int
-    let posterPath: String
-    let releaseDate: String
-    let title: String
-    let voteAverage: Float
-    let voteCount: Int
+    let adult: Bool?
+    let backdropPath: String?
+    let id: Int?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let voteAverage: Float?
+    let voteCount: Int?
 }
