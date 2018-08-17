07e1c5df8383bf0114e2bbc1a28f3c0667ed31e2
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2018-08-17 01:59:25 -0400

    Add UIColor extension for purple color

---
+++ MovieApp/UIColorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColorExtension.swift
+//  MovieApp
+//
+
+import UIKit
+
+extension UIColor {
+    static var purpleColor: UIColor {
+        return UIColor(red:0.50, green:0.37, blue:0.89, alpha:1.0)
+    }
+}
