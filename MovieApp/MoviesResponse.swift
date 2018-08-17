008b7c3c55cd9995f93d1861e606a502d56a9894
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2018-08-16 22:00:26 -0400

    Create decodable MoviesResponse for JSON parsing

---
+++ MovieApp/MoviesResponse.swift
@@ -0,0 +1,19 @@
+//
+//  MoviesResponse.swift
+//  MovieApp
+//
+
+struct MoviesResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let adult: Bool
+    let backdropPath: String
+    let id: Int
+    let posterPath: String
+    let releaseDate: String
+    let title: String
+    let voteAverage: Float
+    let voteCount: Int
+}
