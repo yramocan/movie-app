acfe85d6e7042e616e0f85b8bd98e51f99bc24d1
Yuri Ramocan <yuri.ramocan@detroitlabs.com>
2018-08-16 22:50:54 -0400

    Implement MoviesViewModelDelegate methods

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -5,14 +5,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     private let viewModel = MoviesViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
+        viewModel.delegate = self
+        viewModel.getNowPlaying()
+    }
 }
 
+extension ViewController: MoviesViewModelDelegate {
+    func didEncounterError(_ error: Error) {
+        print("Error")
+
+        let alertController = UIAlertController(title: "MovieApp Error",
+                                                message: "Could not retrieve movies.",
+                                                preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    func didRetrieveMovies() {
+        print("Success")
+        print("Movie Count: \(viewModel.movies.count)")
+    }
+}
